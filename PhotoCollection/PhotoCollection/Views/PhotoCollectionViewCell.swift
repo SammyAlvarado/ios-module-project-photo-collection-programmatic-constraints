4c704accf17f784090eba6aff817e841f7ad7ccf
Sammy Alvarado <isammyalvarado@gmail.com>
2020-07-22 23:14:48 -0400

    Added UIView Constraints
    Created programmatic constraints to my image view

--- PhotoCollection/PhotoCollection/Views/PhotoCollectionViewCell.swift
+++ PhotoCollection/PhotoCollection/Views/PhotoCollectionViewCell.swift
@@ -11,6 +11,60 @@ import UIKit
 class PhotoCollectionViewCell: UICollectionViewCell {
 
     var photo: Photo?
-    
+    var imageView = UIImageView()
+    var photoLabel = UILabel()
+
+
+    func updateViews() {
+        guard let photo = photo else { return }
+
+        imageView.image = UIImage(data: photo.imageData)
+        photoLabel.text = photo.title
+    }
+
+    func setUPSubviews() {
+        imageView.translatesAutoresizingMaskIntoConstraints = false
+        imageView.contentMode = .scaleAspectFit
+
+        addSubview(imageView)
+
+        // This is the Y Positions
+        NSLayoutConstraint(item: imageView,
+                           attribute: .top,
+                           relatedBy: .equal,
+                           toItem: self,
+                           attribute: .top,
+                           multiplier: 1,
+                           constant: 4).isActive = true
+
+        // This is the X Position
+        NSLayoutConstraint(item: imageView,
+                           attribute: .leading,
+                           relatedBy: .equal,
+                           toItem: self,
+                           attribute: .leading,
+                           multiplier: 1,
+                           constant: 4).isActive = true
+
+        // This is th Width
+         NSLayoutConstraint(item: imageView,
+                                  attribute: .trailing,
+                                  relatedBy: .equal,
+                                  toItem: self,
+                                  attribute: .trailing,
+                                  multiplier: 1,
+                                  constant: -4).isActive = true
+        // This is the Height
+        NSLayoutConstraint(item: imageView,
+                                  attribute: .height,
+                                  relatedBy: .equal,
+                                  toItem: imageView,
+                                  attribute: .width,
+                                  multiplier: 1,
+                                  constant: 0).isActive = true
+
+
+    }
+
     
 }
